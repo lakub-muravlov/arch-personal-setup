f5f5de51824359bde4dfc97be9f3983978d633fd
Andrii Muravlov <lakub.muravlov@gmail.com>
2024-10-08 21:37:08 +0300

    *

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ sudo pacman -Syu
 
 cp .zshrc ~/.zshrc
 
+echo "Installing necessary packages"
+
 if command -v git &>/dev/null; then
   echo "Git v$(git -v | awk '{print $3}') is already installed in your system"
 else
@@ -17,13 +19,19 @@ else
     git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ..
 fi
 
+echo "Installing necessary packages..."
+
 yay -S dbeaver gitkraken jmeter storageexplorer postman-bin --noconfirm
+
+echo "Cleaning up the system to escape vscode/storageexplorer debug packages conflicts"
 
 yay -Sc --noconfirm
 
 sudo pacman -Rs $(pacman -Qtdq) --noconfirm
 
-yay -S visual-studio-code-bin zed wine wine-mono wine-gecko bottles steam blender gimp gwenview inkscape kcolorchooser krita okular spectacle timeshift element-desktop brave-bin filezilla librewolf-bin signal-desktop telegram-desktop thunderbird torbrowser-launcher transmission-qt vesktop-bin discord qbittorrent epiphany audacity handbrake kdenlive obs-studio vlc spotify logseq-desktop-bin onlyoffice-bin bleachbit btop gparted partitionmanager nvtop bitwarden neovim vim kweather ttf-droid ttf-ms-fonts noto-fonts ttf-croscore ttf-roboto noto-fonts-cjk noto-fonts-emoji noto-fonts-extra korganizer kdepim-addons fzf zoxide qt5-quickcontrols2 qt5-graphicaleffects qt5-xmlpatterns fastfetch yt-dlp gdu nvidia-open networkmanager-openvpn cava zen-browser-bin android-tools ffmpeg zsh --noconfirm
+echo "Installing necessary packages..."
+
+yay -S visual-studio-code-bin zed wine wine-mono wine-gecko bottles steam blender gimp gwenview inkscape kcolorchooser krita okular spectacle timeshift element-desktop brave-bin filezilla librewolf-bin signal-desktop telegram-desktop thunderbird torbrowser-launcher transmission-qt vesktop-bin discord qbittorrent epiphany audacity handbrake kdenlive obs-studio vlc spotify logseq-desktop-bin onlyoffice-bin bleachbit btop gparted partitionmanager nvtop bitwarden neovim vim kweather ttf-droid ttf-ms-fonts ttf-jetbrains-mono noto-fonts ttf-croscore ttf-roboto noto-fonts-cjk noto-fonts-emoji noto-fonts-extra korganizer kdepim-addons fzf zoxide qt5-quickcontrols2 qt5-graphicaleffects qt5-xmlpatterns fastfetch yt-dlp gdu nvidia-open networkmanager-openvpn cava zen-browser-bin android-tools ffmpeg zsh --noconfirm
 
 echo "Creating bin directory in home"
 
